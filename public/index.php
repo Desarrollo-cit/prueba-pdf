f3d597e1a9fb2bf5d7e193743be876618caccd3c
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2023-09-12 10:07:50 -0600

    Implementación de envio de correo

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\EmailController;
 use Controllers\ReporteController;
 
 $router = new Router();
@@ -11,6 +12,7 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
 $router->get('/pdf', [ReporteController::class,'pdf']);
+$router->get('/email', [EmailController::class,'email']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/email/index.php
@@ -0,0 +1,2 @@
+<h1>Email</h1>
+<p>Esto es un correo de prueba</p>
